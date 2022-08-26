f1146d5199dcd6100353c9c5745dd3a21323edbe
Macrob <se@xambo.io>
2022-08-26 09:54:05 +0300

    fix

--- restart-tds.sh
+++ restart-tds.sh
@@ -5,6 +5,10 @@ rm -r $HOME/logs/*
 rm -r $HOME/ledger/*
 rm -r $HOME/snapshots/*
 
+mkdir -p $HOME/logs
+mkdir -p $HOME/ledger
+mkdir -p $HOME/snapshots/
+
 cd $HOME/snapshots/ && wget --trust-server-names http://api.testnet.solana.com/snapshot.tar.bz2
 cd $HOME/ledger/ && wget --trust-server-names http://api.testnet.solana.com/genesis.tar.bz2
 sleep 1
